9a521de62b8fa7d051d2ac344e80f55e998cca2a
Tibor Fazekas <tibcso0322@gmail.com>
2023-04-19 14:33:16 +0200

    Added actions, save team, delete team, login"

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,52 @@ import React, { useState, useEffect } from 'react'
 const saveToLs: boolean = true
 
 function App(): JSX.Element {
-  const [user, setUser] = useState<User>();
-  const [team, setTeam] = useState<Team>();
+  const [user, setUser] = useState<User | null>(null);
+  const [team, setTeam] = useState<Team | null>(null);
   const [token, setToken] = useState<string>('')
-  const [teamInput, setTeamInput] = useState<{ name: string, location: string, contactEmail: string }>({name: '', location: '', contactEmail: ''});
 
-  const login = async(token: string): Promise<void> => {
-    let data: Awaited<Promise<{ status: string, user: User, team: Team }>> = await (await fetch('http://localhost/login', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify({ token })
-    })).json()
-    setUser(data.user)
-    if(saveToLs) localStorage.setItem('token', data.user.token.toString())
-    data.team = await (await fetch('http://localhost/teams/' + data.user.teamId, { headers: new Headers({ Authorization: data.user.token.toString() }) })).json()
-    console.log(data)
-    
+  const actions = {
+    login: async(token: string): Promise<void> => {
+      let data: Awaited<Promise<{ status: string, user: User, team: Team }>> = await (await fetch('http://localhost/login', {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+        body: JSON.stringify({ token })
+      })).json()
+      if(saveToLs) localStorage.setItem('token', data.user.token.toString())
+      data.team = await (await fetch('http://localhost/teams/' + data.user.teamId, { headers: new Headers({ Authorization: data.user.token.toString() }) })).json()
+      setUser(data.user)
+      setTeam(data.team)
+      console.log(data)
+    },
+
+    saveTeam: async() => {
+      const res = await (await fetch('http://localhost/teams/' + team?.id, {
+        method: 'PUT',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Authorization: user!.token.toString()
+        }),
+        body: JSON.stringify({ name: team!.name, location: team!.location, contactEmail: team!.contactEmail }) }))
+        .json()
+    },
+
+    deleteTeam: async() => {
+      const res = (await fetch('http://localhost/teams/' + team!.id, {
+        method: 'DELETE',
+        headers: new Headers({
+          Authorization: user!.token.toString()
+        })
+      })).json()
+      console.log(res)
+      setUser(null)
+    }
   }
 
   useEffect(() => {
     const fetchData = async(): Promise<void> => {
       const token = localStorage.getItem('token')
       if(!token) return
-      await login(token)
+      await actions.login(token)
     }
 
     fetchData()
@@ -41,18 +64,22 @@ function App(): JSX.Element {
           </div>
           <div className='w-full h-1/2 flex flex-col justify-center items-center gap-2'>
             <input className='w-[80%] h-8 border-2 border-black' placeholder='Token' type="text" onChange={e => setToken(e.target.value)}/>
-            <button onClick={() => login(token)} type='button' className='w-16 h-8 bg-gray-600 border-2 border-black'>Login</button>
+            <button onClick={() => actions.login(token)} type='button' className='w-16 h-8 bg-gray-600 border-2 border-black'>Login</button>
           </div>
         </div>
       </> : <>
         <div className='w-5/6 h-5/6 border-2 border-black rounded-md center flex flex-col justify-center items-center'>
           <div className='w-[95%] h-1/2 border-b-2 border-b-black'>
-            <div className='w-1/3 h-full flex flex-col'>
+            <div className='w-1/3 h-full flex flex-col border-2 justify-evenly items-start'>
               {/** input for teamName contactEmail location */}
-              <h1>Manage your team</h1>
-              <input type="text" className='' onChange={(e) => setTeamInput(prev => ({...prev, name: e.target.value}))}/>
-              <input type="text" onChange={(e) => setTeamInput(prev => ({...prev, contactEmail: e.target.value}))}/>
-              <input type="text" onChange={(e) => setTeamInput(prev => ({...prev, location: e.target.value}))}/>
+              <h1 className='font-semibold text-2xl'>Manage your team</h1>
+              <input type="text" className='w-full h-8 border-2 border-black rounded-sm' value={team?.name} onChange={(e) => setTeam((prev: any) => ({ ...prev, name: e.target.value }))} />
+              <input type="text" className='w-full h-8 border-2 border-black rounded-sm' value={team?.contactEmail || ''} onChange={(e) => setTeam((prev: any) => ({...prev, contactEmail: e.target.value}))}/>
+              <input type="text" className='w-full h-8 border-2 border-black rounded-sm' value={team?.location || ''} onChange={(e) => setTeam((prev: any) => ({...prev, location: e.target.value}))}/>
+              <div className='w-full h-8 flex justify-end gap-4'>
+                <button className='w-24 h-full bg-rose-400 border-2 border-rose-800' type='button'>Delete team</button>
+                <button className='w-12 h-full bg-gray-600 border-2 border-gray-800' type='button' onClick={actions.saveTeam}>Save</button>
+              </div>
             </div>
           </div>
           <div className='w-[95%] h-1/2'>
